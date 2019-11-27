d3ad66260f1d209220a8c82f9b0710b042dd850f
Coon, Ethan T <coonet@ornl.gov>
2019-11-27 14:53:01 -0500

    updates to what I think the reader interface likely will look like.

--- tests/tests_c_c/readers_decl.hh
+++ tests/tests_c_c/readers_decl.hh
@@ -1,44 +1,37 @@
 // Example of what the reader interface might look like?
-
-#include "array.hh"
-
 namespace ELM {
 namespace IO {
 
 //
-// Returns shape in file, as a pair (ntimes, NX_GLOBAL, NY_GLOBAL)
+// Returns shape in file, as a pair (N_TIMES, NX_GLOBAL, NY_GLOBAL)
 //
-std::pair<std::size_t, std::size_t, std::size_t> get_dimensions(const std::string& fname, std::size_t start_year, std::size_t start_months, std::size_t n_months);
-
-//
-// Returns number of phenology times (likely always 12?)
-//
-std::size_t
-get_phenology_num_times(const std::string& fname);
-
-//
-// Assumes shape:
-//  ( get_phenology_num_times(), NX, NY, N_PFTS )
-//
-// offset provides an offset in the first dimension (num_times) to allow
-// reading in multiple years in multiple files via sequential calls.
+std::tuple<std::size_t, std::size_t, std::size_t>
+get_dimensions(const std::string& fname,
+               std::size_t start_year, std::size_t start_months, std::size_t n_months);
 
 
-// I have to think about this one
-void read_phenology(const std::string& fname,
-                    std::size_t offset,
-                    Array<4,double>& lai,
-                    Array<4,double>& sai);
+//
+// Read a variable from the phenology file.
+//
+// Assumes shape(arr) == (N_MONTHS, NX_LOCAL, NY_LOCAL, N_PFT)
+//  
+template<class Array_t>
+void
+read_phenology(const std::string& fname, const std::string& phenology_type,
+               std::size_t start_year, std::size_t start_month,
+               std::size_t i_beg, std::size_t j_beg, Array_t arr);
+
 
 //
-// Assumes shape:
-//   ( get_forcing_num_times(), NX, NY )
+// Read a variable from the forcing files.
 //
-void read_forcing(const std::string& fname, std::size_t start_year, std::size_t start_months, std::size_t n_months, std::size_t i_beg, std::size_t j_beg);
+// Assumes shape(arr) == (N_TIMES, NX_LOCAL, NY_LOCAL )
+//
+template<class Array_t>
+void
+read_forcing(const std::string& fname, const std::string& forcing_type,
+             std::size_t start_year, std::size_t start_month,
+             std::size_t i_beg, std::size_t j_beg, Array_t& forcing);
 
-	
-
-	//I would need nx, ny and times from shape
-	//temperature and precipitation (rain, snow) inside this subroutine
 } // namespace
 } // namespace
