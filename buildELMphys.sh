dcc882cd72ac2fa977c1f74bce0d98b92bbcdfc7
Joe Beisman <jjbeisman@gmail.com>
2020-11-12 02:30:37 -0700

    fixed error in build script

--- buildELMphys.sh
+++ buildELMphys.sh
@@ -3,8 +3,8 @@ if [ -d "build" ]; then
 fi
 ORIGIN_DIR=${pwd}
 mkdir "build" ; cd "build"
-cmake ../
-  -DCMAKE_CXX_FLAGS="-W -Wall -Wextra" \
+cmake .. \
+  -DCMAKE_CXX_FLAGS="-W -Wall -Wextra"
 make VERBOSE=1
 make install
 cd $ORIGIN_DIR
